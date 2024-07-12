f0e6d907907b5441a4163d6ada99766d5c5e6a7e
liuhongda <86993849+liuhongda91@users.noreply.github.com>
2024-07-12 11:32:54 +0800

    Update vpn1.sh

--- vpn1.sh
+++ vpn1.sh
@@ -13,7 +13,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 docker pull hwdsl2/ipsec-vpn-server
 
