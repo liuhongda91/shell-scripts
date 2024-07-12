9c823e054afe7df61f8ba7b021bfa7e57d691e8c
liuhongda <86993849+liuhongda91@users.noreply.github.com>
2024-07-12 11:27:05 +0800

    Create vpn1.sh

---
+++ vpn1.sh
@@ -0,0 +1,19 @@
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+docker pull hwdsl2/ipsec-vpn-server
+
+docker run --restart always --name ipsec-vpn-server -e VPN_IPSEC_PSK=aa123456 -e VPN_USER=user -e VPN_PASSWORD=aa123456 -p 500:500/udp -p 4500:4500/udp -d --privileged hwdsl2/ipsec-vpn-server
