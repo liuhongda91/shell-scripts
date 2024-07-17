a5b7c4d0517f90c578abfcb25311628e65de6973
liuhongda <86993849+liuhongda91@users.noreply.github.com>
2024-07-17 13:49:45 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -10,7 +10,7 @@ yum install wget -y
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo systemctl start docker
 sudo systemctl enable docker
